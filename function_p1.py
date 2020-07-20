9233d1a16d89f685b119e66be452f6acacbb702d
= <=>
2020-07-20 11:22:24 +0900

    add function

--- function_p1.py
+++ function_p1.py
@@ -1,1 +1,11 @@
-#keyword Arguments
+def fourthPower(x):
+    """
+    x: int or float
+    """
+    # I have to practice like this style
+    return square(square(x))
+
+def square(x):
+    return x * x
+
+print(fourthPower(3))
