8804cc27bc254561d9619586b85bea9322775247
= <mrbartrns@naver.com>
2020-07-26 09:34:44 +0900

    add problem set 2

--- problem_set2-2.py
+++ problem_set2-2.py
@@ -1,14 +1,15 @@
 def solve(balance, annualInterestRate):
-    '''
-    Monthly interest rate = (Annual interest rate) / 12.0
-    Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
-    Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-    한달 내는 금액을 열 두번내어 balance를 0으로 만드는 금액> 정답
-    balance를 더하여 그것을 12로 나누면?
-    '''
-    minimumFixedMonthlyPayment = 0
+    minimumFixedMonthlyPayment = 10
+    original = balance
     monthlyInterestRate = annualInterestRate / 12.0
     monthlyUnpaidBalance = balance - minimumFixedMonthlyPayment
-    balance = (monthlyUnpaidBalance) * (1 + monthlyInterestRate)
-    for i in range(12):
-        pass
+    while balance > 0:
+        for i in range(12):
+            monthlyUnpaidBalance = balance - minimumFixedMonthlyPayment
+            balance = monthlyUnpaidBalance * (1 + monthlyInterestRate)
+            i += 1
+        if balance > 0:
+            balance = original
+            minimumFixedMonthlyPayment += 10
+    print("Lowest Payment:", minimumFixedMonthlyPayment)
+
